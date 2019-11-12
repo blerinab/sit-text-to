769d5ab3d867982ca100d7a8951c1efb560f48a5
Blerina Berisha <blerinapberisha@gmail.com>
2019-11-12 20:58:07 +0100

    Initialise project

---
+++ Sit Next To/Sit Next To/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sit Next To
+//
+//  Created by Blerina Berisha on 11/12/19.
+//  Copyright © 2019 Blerina Berisha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
