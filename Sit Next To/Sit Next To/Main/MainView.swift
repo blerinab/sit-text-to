89c5919c7166983901074a066f65215127bd9beb
Blerina Berisha <blerinapberisha@gmail.com>
2019-11-12 23:11:33 +0100

    Setup MainViewController (and all views)

---
+++ Sit Next To/Sit Next To/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Sit Next To
+//
+//  Created by Blerina Berisha on 11/12/19.
+//  Copyright © 2019 Blerina Berisha. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    @IBOutlet private weak var numberTextField: UITextField!
+    
+    @IBAction private func randomize(_ sender: UIButton) {
+        // TODO:
+    }
+}
---
+++ Sit Next To/Sit Next To/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Sit Next To
+//
+//  Created by Blerina Berisha on 11/12/19.
+//  Copyright © 2019 Blerina Berisha. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
