baaea2b2090c7cd83c32f3da6681762ae0a918b1
Blerina Berisha <blerinapberisha@gmail.com>
2019-11-20 20:21:06 +0100

    Add Google Ads pod

---
+++ Sit Next To/Pods/Target Support Files/Pods-Sit Next To-Sit Next ToUITests/Pods-Sit Next To-Sit Next ToUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sit_Next_To_Sit_Next_ToUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Sit_Next_To_Sit_Next_ToUITests
+@end
---
+++ Sit Next To/Pods/Target Support Files/Pods-Sit Next To/Pods-Sit Next To-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sit_Next_To : NSObject
+@end
+@implementation PodsDummy_Pods_Sit_Next_To
+@end
